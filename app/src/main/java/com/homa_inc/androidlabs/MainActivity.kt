97e1ee641ff7f585c28bdfea078215abc802d251
Philipp Hamitsevich <spritefok@gmail.com>
2018-12-01 12:45:23 +0300

    Move about action from bottom navigation to AppBar

--- app/src/main/java/com/homa_inc/androidlabs/MainActivity.kt
+++ app/src/main/java/com/homa_inc/androidlabs/MainActivity.kt
@@ -2,6 +2,8 @@ package com.homa_inc.androidlabs
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
@@ -19,6 +21,19 @@ class MainActivity : AppCompatActivity() {
         bottomNavigation = findViewById(R.id.bottom_navigation)
         navController  = Navigation.findNavController(this@MainActivity, R.id.nav_host_fragment)
         bottomNavigation?.setupWithNavController(navController as NavController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.appbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            R.id.appbar_item_about -> navController?.navigate(R.id.aboutFragment)
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
